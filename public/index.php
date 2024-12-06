dfb2ebcace95ab00dcd03ebca5639d48e2482fdd
DediHar11 <dedi.fast50@gmail.com>
2024-12-07 05:18:37 +0700

    kolang bacin

--- public/index.php
+++ public/index.php
@@ -46,21 +46,21 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// $kernel = $app->make(Kernel::class);
-
-// $response = $kernel->handle(
-//     $request = Request::capture()
-// )->send();
-
-// $kernel->terminate($request, $response);
-
-
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
+    $request = Request::capture()
+)->send();
 
 $kernel->terminate($request, $response);
+
+
+// $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+// $response = $kernel->handle(
+//     $request = Illuminate\Http\Request::capture()
+// );
+
+// $response->send();
+
+// $kernel->terminate($request, $response);
